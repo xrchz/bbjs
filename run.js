20f458da8d5229c5fce3db5588380fae176a486f
Ramana Kumar <ramana@member.fsf.org>
2023-05-21 22:17:18 +0100

    Add main loop

--- run.js
+++ run.js
@@ -10,6 +10,7 @@ program
   .option('-t, --txns <n>', 'average number of transactions to aim to submit per block', '2')
   .option('-c, --contract <addr>', 'transaction recipient', '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2')
   .option('-f, --fee-mult <n>', 'base fee multiplier', '2')
+  .option('-g, --gas-mult <n>', 'gas limit multiplier', '2')
   .option('-p, --priority-fee <gwei>', 'max priority fee per gas', '10')
   .option('-m, --max-txns <n>', 'maximum transactions to submit per block', '8')
   .requiredOption('-s, --signer <key>', 'private key of account to send transactions from')
@@ -28,12 +29,13 @@ let txnsLeft = parseInt(options.txns)
 const maxTxns = parseInt(options.maxTxns)
 const sizeBytes = parseInt(options.size) * 1024 - parseInt(options.trimBytes)
 const feeMult = BigInt(options.feeMult)
+const gasMult = BigInt(options.gasMult)
 const prioFee = ethers.parseUnits(options.priorityFee, 'gwei')
 
-function makeTxn(baseFee, nonce) {
+function makeTxn(maxFee, nonce) {
   const tx = new ethers.Transaction()
   tx.chainId = network.chainId
-  tx.maxFeePerGas = baseFee * feeMult
+  tx.maxFeePerGas = maxFee
   tx.maxPriorityFeePerGas = prioFee
   tx.nonce = nonce
   tx.to = options.contract
@@ -41,12 +43,42 @@ function makeTxn(baseFee, nonce) {
   return tx
 }
 
+const shortHash = (hash) => `${hash.substring(0, 4)}..${hash.substring(hash.length - 4)}`
+const nonces = new Map()
+
+const submitted = []
 provider.on('block', async (blockNumber) => {
+  while (submitted.length && Promise.race([submitted[0], false])) {
+    const receipt = await submitted.shift()
+    console.log(`${nonces.get(receipt.hash)} (${shortHash(receipt.hash)}) included in ${receipt.blockNumber}`)
+  }
+  if (!txnsLeft || !blocksLeft) {
+    if (submitted.length) return
+    else process.exit(0)
+  }
   console.log(`Got block ${blockNumber}`)
   const block = await provider.getBlock(blockNumber)
   const baseFee = block.baseFeePerGas
   console.log(`Base fee: ${ethers.formatUnits(baseFee, 'gwei')} gwei`)
-  const tx = makeTxn(baseFee, 1)
+  const fastFee = baseFee * feeMult
+  console.log(`Fast fee: ${ethers.formatUnits(fastFee, 'gwei')} gwei`)
+  const startingNonce = await signer.getNonce()
+  const toSubmit = Math.min(maxTxns, Math.trunc(txnsLeft / blocksLeft))
+  let gasLimit = null
+  for (const i of Array(toSubmit).keys()) {
+    const tx = makeTxn(fastFee, startingNonce + i)
+    if (!gasLimit) {
+      tx.gasLimit = block.gasLimit
+      gasLimit = gasMult * await signer.estimateGas(tx)
+    }
+    tx.gasLimit = gasLimit
+    const popTx = await signer.populateTransaction(tx)
+    const signedTx = await signer.signTransaction(popTx)
+    const response = await provider.broadcastTransaction(signedTx)
+    console.log(`Submitted ${response.nonce} as ${shortHash(response.hash)}`)
+    nonces.set(response.hash, response.nonce)
+    submitted.push(response.wait())
+    txnsLeft -= 1
+  }
   blocksLeft -= 1
-  if (!txnsLeft || !blocksLeft) process.exit(0)
 })
